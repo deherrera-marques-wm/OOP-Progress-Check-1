53f427180c6fe7838619ed5ec6f79eefd8638daa
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-09-16 14:23:31 -0700

    First commit of progress check

---
+++ Classes.php
@@ -0,0 +1,16 @@
+<?php
+    class car_model {
+        function set_engine(){
+
+        }
+        function get_model(){
+
+        }
+        function set_cpu(){
+
+        }
+        function get_model_cpu(){
+            
+        }
+    }
+?>
---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>OOP Progress Check</title>
+    <?php
+        include_once "Classes.php";
+    ?>
+</head>
+<body>
+<?php
+$obj->set_name("Patrick Clawson");
+?>
+</body>
+</html>
